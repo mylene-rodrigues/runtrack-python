433162b6c9e1bf6b52962ab4894e50393fcbc08a
mylene-rodrigues <115147744+mylene-rodrigues@users.noreply.github.com>
2023-01-06 09:55:02 +0100

    add job01 to job06

---
+++ jour03/job01/nombre.py
@@ -0,0 +1,5 @@
+def vingt():
+    for i in range (0,21):
+        print(i)
+
+vingt()
---
+++ jour03/job02/unnsurdeux.py
@@ -0,0 +1,6 @@
+def vingtbis():
+    for i in range (0,21):
+        if i % 2 ==0:
+            print(i)
+
+vingtbis()
